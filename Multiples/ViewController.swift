da66c6b19f32ee8ba501ae9de38433bf95ff2f71
Alexander Buessing <Alex@dhcp-v880-14-00239.1Xwireless.unc.edu>
2015-10-27 10:11:38 -0400

    Saved Running Version of Multiples

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,9 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Other Variables
+    var sumValue = 0
+    var cumulativeValue = 0
+    
+    //Outlet Variables
+    @IBOutlet var textBox: UITextField!
+    @IBOutlet var displayLabel: UILabel!
+    @IBOutlet var playLabel: UIButton!
+    @IBOutlet var addLabel: UIButton!
+
+    //Buttons
+    @IBAction func playButton(sender: AnyObject) {
+        
+        if textBox.text != nil && textBox.text != "" {
+            
+            textBox.hidden = true
+            playLabel.hidden = true
+            displayLabel.hidden = false
+            addLabel.hidden = false
+            
+            updateLabel()
+            
+        }
+        
+    }
+    
+    @IBAction func addButton(sender: AnyObject) {
+        
+        cumulativeValue += Int(textBox.text!)!
+        
+        if sumValue < 50 {
+            updateLabel()
+        } else {
+            restartGame()
+        }
+        
+    }
+
+    func updateLabel() {
+        
+        sumValue = cumulativeValue + Int(textBox.text!)!
+        
+        displayLabel.text = "\(Int(textBox.text!)!) + \(cumulativeValue) = \(sumValue)"
+    
+    }
+    
+    func restartGame () {
+        
+        textBox.hidden = false
+        displayLabel.hidden = true
+        playLabel.hidden = false
+        addLabel.hidden = true
+        
+        sumValue = 0
+        cumulativeValue = 0
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textBox.hidden = false
+        displayLabel.hidden = true
+        playLabel.hidden = false
+        addLabel.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
